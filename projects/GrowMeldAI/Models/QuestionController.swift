8e949a384084870efe62c3addbe24ec75651232b
Andreas Ott <ai-factory@ott-gruppe.com>
2026-04-06 16:54:56 +0200

    [Mac Factory] Repair iter 2: 436 errors remaining

--- projects/GrowMeldAI/Models/QuestionController.swift
+++
@@ -1,7 +0,0 @@
-// Keep for clarity when context unclear
-class QuestionController { }  // or just Question (if in Views/ folder context)
-
-// Simpler names in organized folders
-Views/Questions/Controller.swift         // Context makes ViewModel obvious
-Views/Profile/Controller.swift
-Views/Exam/Simulation/Controller.swift
