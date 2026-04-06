122c398f4a068f7ffbea9efe727816825b7493fc
Andreas Ott <ai-factory@ott-gruppe.com>
2026-04-06 17:16:29 +0200

    [Mac Factory] Repair iter 1: 842 errors remaining

--- projects/GrowMeldAI/Models/AIFallbackServiceTests.swift
+++
@@ -1,7 +0,0 @@
-// Tests/Services/AIFallbackServiceTests.swift
-class AIFallbackServiceTests: XCTestCase {
-    func testCacheHitReturnsImmediately() async throws { }
-    func testDegradedNetworkFallsToHeuristic() async throws { }
-    func testStaticContentAvailableOffline() async throws { }
-    func testTierSelectionRespectsPriority() async throws { }
-}
